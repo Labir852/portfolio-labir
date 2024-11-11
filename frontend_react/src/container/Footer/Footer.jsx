bd21e7e615c952a1df90695c76242f5432fdb95f
Tanvir Anjum Labir <tanvir.labir@gmail.com>
2024-11-11 15:58:11 +0600

    new build with swal

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -9,7 +9,6 @@ const Footer = () => {
 
   const [formData, setFormData] = useState({name:'',email:'',message:''});
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-
   const [loading, setLoading] = useState(false);
   const [isSentMessage,setIsSentMessage] = useState("");
 
